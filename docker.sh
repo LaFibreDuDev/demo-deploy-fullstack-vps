64df751168999b084c25efc3039cae7b84194f58
LaFibreDuDev <yannick.kuhn@oclock.io>
2024-12-27 00:39:36 +0100

    Mises à jour

--- docker.sh
+++ docker.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
 # arrêter les images docker en cours
-docker compose down
+docker compose -f docker-compose.prod.yml down
 
 # lancer docker en mode background
 # en rebuildant les images
-docker compose up --build -d
+docker compose -f docker-compose.prod.yml up --build -d
+# Pour voir les logs en direct
+docker compose -f docker-compose.prod.yml up --build
+
+
+# Pour voir les logs d'un conteneur
+docker logs backend
+
+# Pour se connecter dans un conteneur
+docker exec -ti backend sh 
+docker exec -ti frontend sh 
